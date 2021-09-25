5c5ccb677059cb9c6bb210f314db6301a77bed91
masum691 <masumhu691@gmail.com>
2021-09-25 20:54:52 +0600

    dynamic star

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,10 +2,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import './Product.css'
+import Rating from 'react-rating';
 
 const Product = (props) => {
     // console.log(props)
-    const {name, img,seller, price, stock} = props.product;
+    const { name, img, seller, price, stock , star} = props.product;
     const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />
     return (
         <div className="product">
@@ -17,6 +18,12 @@ const Product = (props) => {
                 <p><small>{seller}</small></p>
                 <p>{price}</p>
                 <p>only {stock} left in stock - order soon</p>
+                <Rating className="text-warning mb-3"
+                    initialRating={star}
+                    readonly
+                    emptySymbol="far fa-star"
+                    fullSymbol="fas fa-star"
+                ></Rating><br />
                 <button onClick={() => props.handleAddToCart(props.product)} className="regular-btn">{cartIcon} Add to cart</button>
             </div>
         </div>
